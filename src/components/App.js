4e853466dedd27585ef3e9d411862d79dd431586
anuragkaushik10 <ak10anurag@gmail.com>
2022-05-24 15:58:49 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -2,18 +2,21 @@ import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
-  const [text, setText] = useState("");
+  const [clicked, setClicked] = useState(false);
   function handleClick() {
-    setText(
-      "Hello, I've learnt to use the full-stack evaluation tool.This makes me so happy"
-    );
+    setClicked(!clicked);
   }
   return (
     <div id="main">
       <button id="click" onClick={handleClick}>
         Click me
       </button>
-      <p id="para">{text}</p>
+      {clicked && (
+        <p id="para">
+          Hello, I've learnt to use the full-stack evaluation tool.This makes me
+          so happy
+        </p>
+      )}
     </div>
   );
 }
